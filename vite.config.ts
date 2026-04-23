686a345e38f1f40efc5429bbbfd1986a4b05eedc
Nils Vos <74652788+Vosnils18@users.noreply.github.com>
2026-04-23 12:44:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
+  base: '/Portfolio-Website/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
